659ae4872c62916d22ee3c6da66c1ca01daca463
Pete Kersker <petekersker@gmail.com>
2026-08-05 02:19:10 +0000

    feat: setup Garden Tree doctype

--- mylife/hooks.py
+++ mylife/hooks.py
@@ -129,7 +129,9 @@ app_license = "mit"
 # permission_query_conditions = {
 # 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
 # }
-#
+permission_query_conditions = {
+    "Garden Tree": "mylife.mylife.doctype.garden_tree.garden_tree.get_permission_query_conditions",
+}
 # has_permission = {
 # 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
